a3cf4b17eb1b84f34e2fac6d1ce114408fb0c6a9
ankithere <ankit0698@gmail.com>
2024-02-28 11:34:07 +0530

    new projects

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,9 +1,17 @@
 export const skills = [
-  'React.js', 'Redux', 'Javascript(ES6+)',
-  'HTML5/CSS', 'State Management', 'Responsive Design',
-  'RESTful APIs', 'React Router', 'Version Control/git',
-  'Web Performance Optimization', 'Testing', 'Build Tools'
-]
+  "React.js",
+  "Redux",
+  "Javascript(ES6+)",
+  "HTML5/CSS",
+  "State Management",
+  "Responsive Design",
+  "RESTful APIs",
+  "React Router",
+  "Version Control/git",
+  "Web Performance Optimization",
+  "Testing",
+  "Build Tools",
+];
 
 export const projects = [
   {
@@ -11,9 +19,18 @@ export const projects = [
     subtitle: "React Hooks",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "https://media.giphy.com/media/8GqctmCjZjSL8StnCy/giphy.gif",
-    link: 'https://github.com/ankit0698/Ankit-portfolio'
-
+    image:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExamlweWRzZnJ0OGRyeTNxMzlzdGdkOGpiMm15cXhqaWlucXE2NmxvayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT0Gqn9yuw8hnPGn5K/giphy.gif",
+    link: "https://github.com/ankit0698/Ankit-portfolio",
+  },
+  {
+    title: "YouTube Clone App",
+    subtitle: "React, Redux and Routing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXprZXBlYWlhZW1xN25iejhobHhjeDFnYnEzYTVlOXd6ZmV6ZG53dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13Nc3xlO1kGg3S/giphy.gif",
+    link: "https://github.com/ankit0698/YoutubeClone",
   },
   {
     title: "Food Delivery App",
@@ -21,8 +38,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "https://media.giphy.com/media/Jv6gfhjNkzJ8k/giphy.gif",
-    link: 'https://github.com/ankit0698/Food-delivery-app'
-
+    link: "https://github.com/ankit0698/Food-delivery-app",
   },
   {
     title: "Tic Tac Toe",
@@ -30,8 +46,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "https://media.giphy.com/media/Awg0daMM4WxO9ee5vF/giphy.gif",
-    link: 'https://github.com/ankit0698/01-0tic-tac-toe-project'
-
+    link: "https://github.com/ankit0698/01-0tic-tac-toe-project",
   },
   {
     title: "Investment Calculator App",
@@ -39,8 +54,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "https://media.giphy.com/media/cJFQJzZxFMhONxDTnt/giphy.gif",
-    link: 'https://github.com/ankit0698/Investment-calculator-app'
-
+    link: "https://github.com/ankit0698/Investment-calculator-app",
   },
   {
     title: "Event Management App",
@@ -48,7 +62,6 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "https://media.giphy.com/media/J6Ukx0azkxCBGUn846/giphy.gif",
-    link: 'https://github.com/ankit0698/event-management-app'
-
+    link: "https://github.com/ankit0698/event-management-app",
   },
-]
+];
