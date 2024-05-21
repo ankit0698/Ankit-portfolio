c60bad14457d70a16b39a42591b01b15cba7f3e5
ankithere <ankit0698@gmail.com>
2024-05-21 20:23:27 +0530

    ui fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ function About() {
     <>
       <div
         id="about"
-        className=" flex px-10 lg:mt-2 pt-12 bg-[url('/bg-image.svg')] min-h-screen md:bg-none  bg-cover  md:flex-row flex-col items-center overflow-x-hidden "
+        className=" flex px-10 lg:mt-2 pt-12 bg-[url('/bg-image.svg')] min-h-screen md:bg-none  bg-cover  md:flex-row flex-col items-center overflow-hidden "
       >
         <div className=" md:w-1/2   md:pr-16  flex flex-col md:items-start text-left mb-8 md:mb-0 md:-mt-36 items-center ">
           <h1 className="text-bold text-2xl font-['Open_Sans'] md:text-3xl mb-4 font-medium text-blue-300 ">
@@ -54,7 +54,7 @@ function About() {
             </a>
           </div>
         </div>
-        <div className=" md:w-5/6   -mt-64 -mr-[280px] ">
+        <div className=" md:w-5/6 lg:max-w-3/4  -mt-64 -mr-[280px] ">
           <img
             className="object-contain  rounded opacity-85"
             alt="hero"
