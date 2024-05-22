16b5d1da5505f4fc5f1538ac71859372c8a5a4ea
ankit-0698 <ankit0698@gmail.com>
2024-05-23 00:30:04 +0530

    about page ui

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ function About() {
     <>
       <div
         id="about"
-        className=" flex px-10 lg:mt-2 pt-12 bg-[url('/bg-image.svg')] min-h-screen md:bg-none  bg-cover  md:flex-row flex-col items-center overflow-hidden "
+        className=" flex px-10 lg:mt-0 pt-12 bg-[url('/bg-image.svg')] min-h-screen md:bg-none  bg-cover  md:flex-row flex-col items-center overflow-hidden "
       >
         <div className=" md:w-1/2   md:pr-16  flex flex-col md:items-start text-left mb-8 md:mb-0 md:-mt-36 items-center ">
           <h1 className="text-bold text-2xl font-['Open_Sans'] md:text-3xl mb-4 font-medium text-blue-300 ">
@@ -54,9 +54,9 @@ function About() {
             </a>
           </div>
         </div>
-        <div className=" md:w-[60%]   -mt-60 lg:-mt-84 -mr-[280px]  ">
+        <div className=" md:w-[60%]  -mt-60 lg:-mt-84 -mr-[280px]  ">
           <img
-            className="object-contain max-w-[900px]  pt-6  overflow-hidden  rounded opacity-75"
+            className="object-contain max-w-[900px] border-l-2 border-b-2  pt-6  overflow-hidden  rounded opacity-75"
             alt="hero"
             src="bg-image.svg"
           />
